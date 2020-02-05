7873ad3ba9f35f7940ce1e953850f83f955f3113
wildenali <wildeeeeen@gmail.com>
2020-02-05 22:48:46 +0700

    Pindahin air dalam ember

---
+++ C 008 - Latihan Pendahuluan/C008e-pindahEmber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    double emberPertama, emberKedua, emberSementara;
+
+    printf("Masukan air ke Ember pertama: ");
+    scanf("%lf", &emberPertama);
+
+    printf("Masukan air ke Ember kedua: ");
+    scanf("%lf", &emberKedua);
+
+    // Pindahin ke emberSementara dulu
+    emberSementara = emberPertama;
+
+    // pindahin emberKedua ke emberPertama
+    emberPertama = emberKedua;
+
+    // emberKedua diisi oleh emberSementara
+    emberKedua = emberSementara;
+
+    printf("\nSetelah di tuker, emberPertama menjadi: %.2lf", emberPertama);
+    printf("\nSetelah di tuker, emberKedua menjadi: %.2lf\n\n", emberKedua);
+
+    return 0;
+}
