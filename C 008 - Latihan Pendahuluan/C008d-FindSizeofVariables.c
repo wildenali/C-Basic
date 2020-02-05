5d9f6edc524c7ec263037a0cad62dd9e4d73907d
wildenali <wildeeeeen@gmail.com>
2020-02-05 22:31:31 +0700

    sizeof dari variable

--- C 008 - Latihan Pendahuluan/C008d-FindSizeofVariables.c
+++ C 008 - Latihan Pendahuluan/C008d-FindSizeofVariables.c
@@ -12,5 +12,16 @@ int main() {
     printf("Size of Double: %ld bytes \n", sizeof(tipeDouble));
     printf("Size of Char: %ld bytes \n", sizeof(tipeChar));
 
+    int a;
+    long b;   // equivalent to long int b;
+    long long c;  // equivalent to long long int c;
+    double e;
+    long double f;
+    printf("Size of int = %ld bytes \n", sizeof(a));
+    printf("Size of long int = %ld bytes\n", sizeof(b));
+    printf("Size of long long int = %ld bytes\n", sizeof(c));
+    printf("Size of double = %ld bytes\n", sizeof(e));
+    printf("Size of long double = %ld bytes\n", sizeof(f));
+
     return 0;
 }
