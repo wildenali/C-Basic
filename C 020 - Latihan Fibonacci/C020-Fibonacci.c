ea30be9135e40edd58273d3a273771f7c3eab4c3
wildenali <wildeeeeen@gmail.com>
2020-02-09 22:48:53 +0700

    latihan deret fibonacci

--- C 020 - Latihan Fibonacci/C020-Fibonacci.c
+++ C 020 - Latihan Fibonacci/C020-Fibonacci.c
@@ -11,10 +11,10 @@ int main() {
     int t1 = 0;
     int t2 = 1;
     int nextTerm;
+    printf("===== Deret Fibonacci =====\n\n");
 
-    printf("Enter the number of terms: ");
+    printf("\n==================== Masukan angka sampai deret ke berapa ====================\n");
     scanf("%d", &n);
-    printf("===== Deret Fibonacci =====\n");
     for (i = 1; i <= n; ++i)
     {
         printf("%d, ", t1);
@@ -22,6 +22,24 @@ int main() {
         t1 = t2;
         t2 = nextTerm;
     }
+    printf("\n==================== Masukan angka sampai deret ke berapa ====================\n\n\n");
+
+
+    printf("\n==================== Fibonacci sampai Nomor yg di Inginkan ====================\n");
+    int m;
+    int s1 = 0;
+    int s2 = 1;
+    int nexTerm = 0;
+    scanf("%d", &m);
+    printf("Fibonacci Ke: %d, %d, ", s1, s2);
+    while (nexTerm <= m)
+    {
+        printf("%d, ", nexTerm);
+        s1 = s2;
+        s2 = nexTerm;
+        nexTerm = s1 + s2;
+    }
+    printf("\n==================== Fibonacci sampai Nomor yg di Inginkan ====================\n");
     
 
 
