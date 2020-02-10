c14510a427729b48b3367aef62d63524f5ec4cdf
wildenali <wildeeeeen@gmail.com>
2020-02-10 15:49:48 +0700

    latihan Faktor Persekutuan terBesar

---
+++ C 021 - Faktor Persekutuan Terbesar/C021-FaktorPersekutuanTerbesar.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/*
+Greater Common Divisor (GCD) atau
+Highest Common Factor (HCF) atau
+Faktor Persekutuan Terbesar (FPB)
+*/
+
+/*
+Mencari FPB dari 12 dan 20:
+
+Faktor dari 12 = 1, 2, 3, 4, 6 dan 12
+Faktor dari 20 = 1, 2, 4, 5, 10 dan 20
+FPB dari 12 dan 20 adalah faktor sekutu (sama) yang terbesar, yaitu 4.
+Mencari FPB dari 15 dan 25:
+
+Faktor dari 15 = 1, 3, 5, dan 15
+Faktor dari 25 = 1, 5, dan 25
+FPB dari 15 dan 25 adalah faktor sekutu (sama) yang terbesar, yaitu 5.
+*/
+
+/*
+Mencari FPB dari bilangan 147, 189 dan 231:
+
+Buat pohon faktor dari masing-masing bilangan:
+             147         189             231
+              /\          /\              /\
+             3 49        3  63           3  77
+               /\           /\              /\
+              7  7         7  9            7  11
+                              /\
+                             3  3
+
+Susun bilangan dari pohon faktor utk mendapatkan faktorialnya:
+Faktorial 147 = 31 x 72
+Faktorial 189 = 33 x 71
+Faktorial 231 = 31 x 71 x 111
+
+Ambil faktor-faktor yang sekutu (sama) dari ketiga faktorial tersebut,
+dalam hal ini 3 dan 7.
+
+Kalikan faktor-faktor sekutu yang memiliki pangkat terkecil,
+dalam hal ini 31 x 71 = 21.
+
+Maka FPB dari bilangan 147, 189 dan 231 adalah 21.
+Dengan kata lain, tidak ada bilangan yang lebih besar dari 21
+yang dapat membagi habis bilangan 147, 189 dan 231.
+*/
+
+int main {
+	
+	
+	
+	return 0;
+}
