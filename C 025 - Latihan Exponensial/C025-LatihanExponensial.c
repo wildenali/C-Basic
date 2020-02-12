b8cc768bc7c035ff74cd2998172b9248281e6940
wildenali <wildeeeeen@gmail.com>
2020-02-12 23:01:14 +0700

    latihan ekponensial

--- C 025 - Latihan Exponensial/C025-LatihanExponensial.c
+++ C 025 - Latihan Exponensial/C025-LatihanExponensial.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include <math.h> // untuk fungsi pow()
 
 int main() {
 
-    int base, exp;
-    long long result = 1;
+    int base1, exp1;
+    long long result1 = 1;
+    double base2, exp2;
+    double result2;
     printf("Enter a base number: ");
-    scanf("%d", &base);
+    scanf("%d", &base1);
     printf("Enter a exponent: ");
-    scanf("%d", &exp);
+    scanf("%d", &exp1);
 
-    while (exp != 0)
+    printf("\n==================== Pake WHILE LOOP ====================\n");
+    while (exp1 != 0)
     {
-        result = result * base;
-        --exp;
+        result1 = result1 * base1;
+        --exp1;
     }
-    printf("Jawabannya adalah %lld", result);
-    
+    printf("Jawabannya adalah %lld", result1);
+    printf("\n==================== Pake WHILE LOOP ====================\n\n\n");
+
+
+    printf("Enter a base number: ");
+    scanf("%lf", &base2);
+    printf("Enter a exponent: ");
+    scanf("%lf", &exp2);
+    printf("\n==================== Pake fungsi pow() ====================\n");
+    result2 = pow(base2, exp2);
+    printf("Jawabannya dari %.1lf^%.1lf adalah %.2lf", base2, exp2, result2);
+    printf("\n==================== Pake fungsi pow() ====================\n");
 
     return 0;
 }
