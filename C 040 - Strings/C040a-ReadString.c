e97718f54f7d11416e3ff3ab9869acdc5ec0c264
wildenali <wildeeeeen@gmail.com>
2020-02-19 19:20:30 +0700

    cara nya membaca string, dan menampilkan kembali

---
+++ C 040 - Strings/C040a-ReadString.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\n=============== Contoh 1 ===============\n");
+    char name[20];
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("Your name is %s", name);
+    printf("\n=============== Contoh 1 ===============\n");
+
+    return 0;
+}
