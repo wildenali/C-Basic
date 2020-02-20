0cb65994eb9f66f4ca834c38a35dd8dfdb06596f
wildenali <wildeeeeen@gmail.com>
2020-02-20 07:25:38 +0700

    cara penulisan struct

---
+++ C 043 - Struct/C043a-DefineTheStruct.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/*
+In C programming, a struct (or structure) is a collection of
+variables (can be of different types) under a single name.
+
+struct structureName 
+{
+    dataType member1;
+    dataType member2;
+    ...
+};
+
+Access members of a structure
+There are two types of operators used for accessing members
+of a structure.
+
+.   - Member operator
+->  - Structure pointer operator (will be discussed in the next tutorial)
+
+*/
+
+// Contoh struct Pertama
+struct Person
+{
+    char name[50];
+    int citNo;
+    float salary;
+};
+
+// Contoh struct Kedua
+struct Orang
+{
+    char name[50];
+    int citNo;
+    float salary;
+} person1, person2, p[20];  // person1 dst adalah struct Orang yg dibuat
+
+int main() {
+
+
+    return 0;
+}
